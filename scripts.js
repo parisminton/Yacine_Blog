bb3b50285ce8d455f0cab42a1a1474389c6eaffa
Faa Diallo <faadiallop@gmail.com>
2017-07-21 14:58:15 -0400

    Initial Commit
    First Time

---
+++ scripts.js
@@ -0,0 +1,7 @@
+/*const posts = [ {"title": "top ten ways to be an ally to minorities",
+"date": "12/12/2000"} ];
+console.log(posts[0].title);*/
+const notcurrentpage = document.getElementsByClassName('notcurrentpage');
+for(var i = 0; i < notcurrentpage.length; i++){
+  notcurrentpage[i].addEventListener('mouseover', this.style.color = '#ffffff');
+}
